dc086d5d4a751c62721e3dc93aa6dd2069ac6b47
teatran <teaprogramming@gmail.com>
2017-12-25 10:51:33 +0700

    add Bootstrap and home.html

--- waitercaller.py
+++ waitercaller.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_login import LoginManager, login_required
+
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 
 @app.route("/")
 def home():
-    return "Under construction"
+    return render_template("home.html")
 
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in."
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
